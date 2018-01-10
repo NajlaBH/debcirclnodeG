0c67d043e15c0e9908a29a83b8660bca62c142a2
NajlaBH-Dev <BHNajla.bioinfo@gmail.com>
2018-01-10 09:31:29 +0000

    Fix deploy file.

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ docker pull bhntools/debcirclnode:latest
 docker stop web || true
 docker rm web || true
 docker rmi bhntools/debcirclnode:current || true
-docker tag bhntools/debcirclnode:latest bhntools/circlenode:current
+docker tag bhntools/debcirclnode:latest bhntools/debcirclnode:current
 docker run -d --net app --restart always --name web -p 3000:3000 -p 4200:4200 bhntools/debcirclnode:current
 EOF
